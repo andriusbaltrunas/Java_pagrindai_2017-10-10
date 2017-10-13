5eb0a469e73354e172433811a8537210926ae5ac
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2017-10-13 19:09:12 +0300

    array list

--- src/days/ketvirta/ListExample.java
+++ src/days/ketvirta/ListExample.java
@@ -1,11 +1,13 @@
 package days.ketvirta;
 
+import java.util.ArrayList;
+
 /**
  * Created by andriusbaltrunas on 10/13/2017.
  */
 public class ListExample {
 
     public static void main(String[] args) {
-        
+        ArrayList<Integer> simpleArray = new ArrayList<>();
     }
 }
